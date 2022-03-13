3086ab1fb3436a07abb072356fbb219dd1063030
Scott Stone <scott.allan.stone@gmail.com>
2022-03-13 14:48:49 -0600

    up to date

--- plot_trajectory.py
+++ plot_trajectory.py
@@ -11,18 +11,20 @@ def main():
         print(f'Loaded file {pkl_file}')
 
         data = np.array(data_raw)
-        fig = plt.figure()
         ax = plt.axes(projection ='3d')
 
         ax.set_xlim(0, 1280)
         ax.set_ylim(0, 720)
         ax.set_xlabel('X (px)')
         ax.set_ylabel('Y (px)')
-        ax.set_zlabel('Time')
+        ax.set_zlabel('Time (frame)')
+        ax.set_title(f'Trajectory of {pkl_file}')
+
         ax.scatter(data[:,1], data[:,2], data[:,0], label=f'Trajectory: {pkl_file}')
 
+        # scatter plot any data outside of the limits as a red dot
+        #ax.scatter(data[:,1][data[:,0] > data[-1,0]], data[:,2][data[:,0] > data[-1,0]], data[:,0][data[:,0] > data[-1,0]], c='r', label='Out of bounds')
         plt.show()
-
 
 if __name__ == "__main__":
     main()
